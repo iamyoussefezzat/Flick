b67f8c3b3cf888ca68369a812877e5ad9dfd51bc
iamyoussefezzat <youssefm.ezzat99@gmail.com>
2025-09-15 03:02:05 +0300

    update env port

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const ENV = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_URI: process.env.MONGO_URI,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
