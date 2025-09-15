2689d499d97b78da02d8edc5f10e35f1d593c86d
iamyoussefezzat <youssefm.ezzat99@gmail.com>
2025-09-16 02:14:30 +0300

    server listen fn update

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,20 @@ import { connectDB } from './config/db.js';
       res.send('Hello World!');
     });
 
-app.listen(ENV.PORT, () => {
-  console.log('Server is running on http://localhost:' + ENV.PORT);
-});    
+    const startServer = async () => {
+      try {
+        await connectDB();
+    
+        // listen for local development
+        if (ENV.NODE_ENV !== "production") {
+          app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+        }
+      } catch (error) {
+        console.error("Failed to start server:", error.message);
+        process.exit(1);
+      }
+    };
+    
+    startServer();
+    
+  
