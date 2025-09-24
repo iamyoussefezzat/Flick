7fce94f4ceba4db6515360905de67671934435be
iamyoussefezzat <youssefm.ezzat99@gmail.com>
2025-09-25 00:56:35 +0300

    add social authentication hooks and integrate with Google and Apple sign-in

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,9 +1,11 @@
-import { View, Image, TouchableOpacity,Text  } from "react-native";
+import { useSocialAuth } from "@/hooks/useSocialAuth";
+import { View, Image, TouchableOpacity,Text , ActivityIndicator } from "react-native";
 
 
 
 export default function Index() {
-  const  isLoading  = false;
+
+  const  {isLoading,handleSocialAuth}  = useSocialAuth();
   return (
     <View className="flex-1  bg-white">
       <View className="flex-1 px-8 justify-between">
@@ -20,10 +22,11 @@ export default function Index() {
 
                            {/* BUTTONS */}
 
-             {/* GOOGLE BUTTON */}              
             <View className="flex-col gap-2">
+
+              {/* GOOGLE BUTTON */}     
               <TouchableOpacity className=" flex-row items-center justify-center bg-white border border-gray-300 rounded-full py-3 px-6"
-                onPress={() => {}}
+                onPress={() => handleSocialAuth("oauth_google")}
                 disabled={isLoading}
                 style={{
                   shadowColor: "#000",
@@ -32,21 +35,27 @@ export default function Index() {
                   shadowRadius: 2,
                   elevation: 2,
                 }}
-              >
+                >
+                    {isLoading ? (
+                  <ActivityIndicator size="small"  color="#4285F4"  />
+                )
+                  :
+                  ( 
                   <View className="flex-row items-center justify-center">
                   <Image
                     source={require("../../assets/images/google.png")}
                     className="size-10 mr-3"
                     resizeMode="contain"
-                  />
+                    />
                   <Text className="text-black font-medium text-base">Continue with Google</Text>
                   </View>
+                   )}         
               </TouchableOpacity>
 
               {/* APPLE BUTTON */}
               <TouchableOpacity 
               className="flex-row items-center justify-center bg-white border border-gray-300 rounded-full py-3 px-6"
-                onPress={() => {}}
+              onPress={() => handleSocialAuth("oauth_apple")}
                 disabled={isLoading}
                 style={{
                   shadowColor: "#000",
@@ -56,20 +65,33 @@ export default function Index() {
                   elevation: 2,
                 }}
                  >
+                  {isLoading ? (
+                     <ActivityIndicator size="small" color="#000" />
+                  ) :
+                  (
 
-                  <View className="flex-row items-center justify-center">
+                    <View className="flex-row items-center justify-center">
                     <Image
                       source={require("../../assets/images/apple.png")}
                       className="size-8 mr-3"
                       resizeMode="contain"
-                    />
+                      />
                     <Text className="text-black font-medium text-base">Continue with Apple</Text>
                   </View>
+                    )}
 
 
                  </TouchableOpacity>
               </View>
 
+                 {/* Terms and Privacy */}
+          <Text className="text-center text-gray-500 text-xs leading-4 mt-6 px-2">
+            By signing up, you agree to our <Text className="text-blue-500">Terms</Text>
+            {", "}
+            <Text className="text-blue-500">Privacy Policy</Text>
+            {", and "}
+            <Text className="text-blue-500">Cookie Use</Text>.
+          </Text>
           </View>   
       </View>
   </View>
