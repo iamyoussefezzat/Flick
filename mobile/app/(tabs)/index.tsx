0d359a93b9cf689c0a4cd930f899e3f5ed4305cc
iamyoussefezzat <youssefm.ezzat99@gmail.com>
2025-10-10 23:54:08 +0300

    Enhance HomeScreen layout with header and icon integration

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
-import SignOutBtn from '@/components/SignOutBtn'
-import { useUserSync } from '@/hooks/useUserSync'
-import { View, Text } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-
+import SignOutBtn from "@/components/SignOutBtn";
+import { useUserSync } from "@/hooks/useUserSync";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { RefreshControl, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 const HomeScreen = () => {
   
   useUserSync()
 
   return (
     <SafeAreaView className='flex-1'>
-      <Text>Home Sc sdfreen</Text>
+      <View className="flex-row justify-between items-center px-4 py-3 border-b border-gray-100">
+        <Ionicons name="logo-twitter" size={24} color="#1DA1F2" />
+        <Text className="text-xl font-bold text-gray-900">Home</Text>
+      
+      </View>
       <SignOutBtn/>
     </SafeAreaView>
   )
