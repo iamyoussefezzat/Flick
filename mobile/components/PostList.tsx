6e9ab09a1dc3c0de1dca238cb79368bbd3ef21f0
iamyoussefezzat <youssefm.ezzat99@gmail.com>
2025-10-11 02:58:54 +0300

    Integrate useCurrentUser hook into PostList component

--- mobile/components/PostList.tsx
+++ mobile/components/PostList.tsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useCurrentUser } from '@/hooks/useCurrentUser'
 
 const PostList = () => {
+  useCurrentUser()
   return (
     <View>
       <Text>PostList</Text>
