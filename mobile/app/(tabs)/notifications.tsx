3e211031f3aae1e8e1b71982d2f847e994fe428d
iamyoussefezzat <youssefm.ezzat99@gmail.com>
2025-10-15 00:56:26 +0300

    notification-tab ✅

--- mobile/app/(tabs)/notifications.tsx
+++ mobile/app/(tabs)/notifications.tsx
@@ -1,28 +1,37 @@
-import NoNotificationsFound from '@/components/noNotificationFound'
-import NotificationCard from '@/components/NotificationCard'
-import { useNotifications } from '@/hooks/useNotifications'
-import { Feather } from '@expo/vector-icons'
-import { View, Text , TouchableOpacity, ScrollView, ActivityIndicator} from 'react-native'
-import { useSafeAreaInsets,SafeAreaView } from 'react-native-safe-area-context'
+import NoNotificationsFound from "@/components/noNotificationFound";
+import NotificationCard from "@/components/NotificationCard";
+import { useNotifications } from "@/hooks/useNotifications";
+import { Notification } from "@/types";
+import { Feather } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
+const NotificationsScreen = () => {
+  const { notifications, isLoading, error, refetch, isRefetching, deleteNotification } =
+    useNotifications();
 
-const notificationScreen = () => {
-    const {notifications, isLoading, error, refetch, deleteNotification} = useNotifications()
-    const insets = useSafeAreaInsets()
+  const insets = useSafeAreaInsets();
 
-    if(error){
-        return (
-            <View className='flex-1 items-center justify-center p-8'>
-                <Text>Error loading notifications</Text>
-                <TouchableOpacity className="bg-blue-500 px-4 py-2 rounded-lg" 
-                onPress={() => refetch()} >
-                    <Text className="text-white">Retry</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+  if (error) {
+    return (
+      <View className="flex-1 items-center justify-center p-8">
+        <Text className="text-gray-500 mb-4">Failed to load notifications</Text>
+        <TouchableOpacity className="bg-blue-500 px-4 py-2 rounded-lg" onPress={() => refetch()}>
+          <Text className="text-white font-semibold">Retry</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white" edges={["top"]}>
       {/* Header */}
       <View className="flex-row items-center justify-between px-4 py-3 border-b border-gray-100">
         <Text className="text-xl font-bold text-gray-900">Notifications</Text>
@@ -31,36 +40,33 @@ const notificationScreen = () => {
         </TouchableOpacity>
       </View>
 
+      {/* CONTENT */}
       <ScrollView
-        contentContainerStyle={{ paddingBottom: insets.bottom }}
         className="flex-1"
+        contentContainerStyle={{ paddingBottom: 100 + insets.bottom }}
         showsVerticalScrollIndicator={false}
-
+        refreshControl={
+          <RefreshControl refreshing={isRefetching} onRefresh={refetch} tintColor={"#1DA1F2"} />
+        }
       >
-        {isLoading ? 
-        ( <View className="flex-1 items-center justify-center p-8">
-          <ActivityIndicator size="large" color="#1DA1F2" />
-          <Text className="text-gray-500 mt-4">Loading notifications...</Text>
-        </View>
+        {isLoading ? (
+          <View className="flex-1 items-center justify-center p-8">
+            <ActivityIndicator size="large" color="#1DA1F2" />
+            <Text className="text-gray-500 mt-4">Loading notifications...</Text>
+          </View>
         ) : notifications.length === 0 ? (
           <NoNotificationsFound />
-        ) : 
-        (
-          <View>
-              <Text>Error loading notifications</Text>
-          </View>
-          // notifications.map((notification: Notification) => (
-          //   <NotificationCard
-          //     key={notification._id}
-          //     notification={notification}
-          //     onDelete={deleteNotification}
-          //   />
-          // )};
-          
+        ) : (
+          notifications.map((notification: Notification) => (
+            <NotificationCard
+              key={notification._id}
+              notification={notification}
+              onDelete={deleteNotification}
+            />
+          ))
         )}
       </ScrollView>
     </SafeAreaView>
-  )
-}
-
-export default notificationScreen
+  );
+};
+export default NotificationsScreen;
